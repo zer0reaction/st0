2fe193901914293fbcedb7a9bb0ae964e9b8768c
zer0reaction <boltgopnik@gmail.com>
2025-02-15 18:23:22 +0300

    Added spaces for visual clarity

--- src/st0.h
+++ src/st0.h
@@ -37,17 +37,17 @@ typedef void st0_list_int32;
 /* uint32 list */
 
 st0_list_uint32* st0_list_uint32_create(uint32_t size);
-void st0_list_uint32_destroy(st0_list_uint32* list_ptr);
-void st0_list_uint32_set(st0_list_uint32* list_ptr, uint32_t pos, uint32_t val);
-uint32_t st0_list_uint32_get(st0_list_uint32* list_ptr, uint32_t pos);
-void st0_list_uint32_append(st0_list_uint32* list_ptr, uint32_t val);
+void             st0_list_uint32_destroy(st0_list_uint32* list_ptr);
+void             st0_list_uint32_set(st0_list_uint32* list_ptr, uint32_t pos, uint32_t val);
+uint32_t         st0_list_uint32_get(st0_list_uint32* list_ptr, uint32_t pos);
+void             st0_list_uint32_append(st0_list_uint32* list_ptr, uint32_t val);
 
 /* int32 list */
 
 st0_list_int32* st0_list_int32_create(uint32_t size);
-void st0_list_int32_destroy(st0_list_int32* list_ptr);
-void st0_list_int32_set(st0_list_int32* list_ptr, uint32_t pos, int32_t val);
-int32_t st0_list_int32_get(st0_list_int32* list_ptr, uint32_t pos);
-void st0_list_int32_append(st0_list_int32* list_ptr, int32_t val);
+void            st0_list_int32_destroy(st0_list_int32* list_ptr);
+void            st0_list_int32_set(st0_list_int32* list_ptr, uint32_t pos, int32_t val);
+int32_t         st0_list_int32_get(st0_list_int32* list_ptr, uint32_t pos);
+void            st0_list_int32_append(st0_list_int32* list_ptr, int32_t val);
 
 #endif
