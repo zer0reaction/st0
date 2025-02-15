2623e86cb93ffad1e0ea1d1e4feb7edf9bbb7840
zer0reaction <boltgopnik@gmail.com>
2025-02-15 17:58:26 +0300

    Added int list

--- src/st0_internal.c
+++ src/st0_internal.c
@@ -34,6 +34,8 @@ list_ptr->data_ptr = realloc(list_ptr->data_ptr, sizeof(*(list_ptr->data_ptr)) *
 (list_ptr->data_ptr)[list_ptr->size] = val; \
 list_ptr->size++;
 
+/* uint32 list */
+
 st0_list_uint32* st0_list_uint32_create(uint32_t size) {
     ST0_MACRO_LIST_CREATE(uint32_t, st0_list_uint32);
 }
@@ -51,5 +53,27 @@ uint32_t st0_list_uint32_get(st0_list_uint32* list_ptr, uint32_t pos) {
 }
 
 void st0_list_uint32_append(st0_list_uint32* list_ptr, uint32_t val) {
+    ST0_MACRO_LIST_APPEND;
+}
+
+/* int32 list */
+
+st0_list_int32* st0_list_int32_create(uint32_t size) {
+    ST0_MACRO_LIST_CREATE(int32_t, st0_list_int32);
+}
+
+void st0_list_int32_destroy(st0_list_int32* list_ptr) {
+    ST0_MACRO_LIST_DESTROY;
+}
+
+void st0_list_int32_set(st0_list_int32* list_ptr, uint32_t pos, int32_t val) {
+    ST0_MACRO_LIST_SET;
+}
+
+int32_t st0_list_int32_get(st0_list_int32* list_ptr, uint32_t pos) {
+    ST0_MACRO_LIST_GET;
+}
+
+void st0_list_int32_append(st0_list_int32* list_ptr, int32_t val) {
     ST0_MACRO_LIST_APPEND;
 }
