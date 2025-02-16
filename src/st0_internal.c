b0b4c711408bc4c13102ffc55496bcdb679545b5
zer0reaction <boltgopnik@gmail.com>
2025-02-16 11:07:22 +0300

    Added _IMPL to macros

--- src/st0_internal.c
+++ src/st0_internal.c
@@ -13,7 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 /* Praise The Machine Spirit for this macro abomination not to break */
 
-#define ST0_MACRO_LIST_CREATE(type, typename) \
+#define ST0_MACRO_LIST_CREATE_IMPL(type, typename) \
 typename* list_ptr = malloc(sizeof(*list_ptr)); \
 int i; \
 list_ptr->size = size; \
@@ -23,11 +23,11 @@ for (i = 0; i < size; i++) { \
 } \
 return list_ptr;
 
-#define ST0_MACRO_LIST_DESTROY \
+#define ST0_MACRO_LIST_DESTROY_IMPL \
 free(list_ptr->data_ptr); \
 free(list_ptr);
 
-#define ST0_MACRO_LIST_SET \
+#define ST0_MACRO_LIST_SET_IMPL \
 if (pos >= list_ptr->size) { \
     fprintf(stderr, "Error in list_set:\n"); \
     fprintf(stderr, "Invalid pos value: pos = %d, size = %d.\n", pos, list_ptr->size); \
@@ -35,7 +35,7 @@ if (pos >= list_ptr->size) { \
 } \
 (list_ptr->data_ptr)[pos] = val;
 
-#define ST0_MACRO_LIST_GET \
+#define ST0_MACRO_LIST_GET_IMPL \
 if (pos >= list_ptr->size) { \
     fprintf(stderr, "Error in list_get:\n"); \
     fprintf(stderr, "Invalid pos value: pos = %d, size = %d.\n", pos, list_ptr->size); \
@@ -43,7 +43,7 @@ if (pos >= list_ptr->size) { \
 } \
 return (list_ptr->data_ptr)[pos];
 
-#define ST0_MACRO_LIST_APPEND \
+#define ST0_MACRO_LIST_APPEND_IMPL \
 list_ptr->data_ptr = realloc(list_ptr->data_ptr, sizeof(*(list_ptr->data_ptr)) * (list_ptr->size + 1)); \
 (list_ptr->data_ptr)[list_ptr->size] = val; \
 list_ptr->size++;
@@ -51,43 +51,43 @@ list_ptr->size++;
 /* uint32 list */
 
 st0_list_uint32* st0_list_uint32_create(uint32_t size) {
-    ST0_MACRO_LIST_CREATE(uint32_t, st0_list_uint32);
+    ST0_MACRO_LIST_CREATE_IMPL(uint32_t, st0_list_uint32);
 }
 
 void st0_list_uint32_destroy(st0_list_uint32* list_ptr) {
-    ST0_MACRO_LIST_DESTROY;
+    ST0_MACRO_LIST_DESTROY_IMPL;
 }
 
 void st0_list_uint32_set(st0_list_uint32* list_ptr, uint32_t pos, uint32_t val) {
-    ST0_MACRO_LIST_SET;
+    ST0_MACRO_LIST_SET_IMPL;
 }
 
 uint32_t st0_list_uint32_get(st0_list_uint32* list_ptr, uint32_t pos) {
-    ST0_MACRO_LIST_GET;
+    ST0_MACRO_LIST_GET_IMPL;
 }
 
 void st0_list_uint32_push_back(st0_list_uint32* list_ptr, uint32_t val) {
-    ST0_MACRO_LIST_APPEND;
+    ST0_MACRO_LIST_APPEND_IMPL;
 }
 
 /* int32 list */
 
 st0_list_int32* st0_list_int32_create(uint32_t size) {
-    ST0_MACRO_LIST_CREATE(int32_t, st0_list_int32);
+    ST0_MACRO_LIST_CREATE_IMPL(int32_t, st0_list_int32);
 }
 
 void st0_list_int32_destroy(st0_list_int32* list_ptr) {
-    ST0_MACRO_LIST_DESTROY;
+    ST0_MACRO_LIST_DESTROY_IMPL;
 }
 
 void st0_list_int32_set(st0_list_int32* list_ptr, uint32_t pos, int32_t val) {
-    ST0_MACRO_LIST_SET;
+    ST0_MACRO_LIST_SET_IMPL;
 }
 
 int32_t st0_list_int32_get(st0_list_int32* list_ptr, uint32_t pos) {
-    ST0_MACRO_LIST_GET;
+    ST0_MACRO_LIST_GET_IMPL;
 }
 
 void st0_list_int32_push_back(st0_list_int32* list_ptr, int32_t val) {
-    ST0_MACRO_LIST_APPEND;
+    ST0_MACRO_LIST_APPEND_IMPL;
 }
