bbc245c5f520dd3640b79626a6d238177827a86a
zer0reaction <boltgopnik@gmail.com>
2025-02-20 19:33:10 +0300

    Started yet another rewrite

--- Makefile
+++ Makefile
@@ -1,11 +1,5 @@
-TESTFLAGS = -Wall -Werror -Wpedantic -std=c89 -fsanitize=address
-STATICFLAGS = -Wall -Werror -Wpedantic -std=c89
+CFLAGS = -Wall -Werror -std=c89 -fsanitize=address
 LFLAGS = -Lbin -lst0
-
-static:
-	@mkdir -p ./bin
-	gcc $(STATICFLAGS) -c -o ./bin/st0.o ./src/st0_internal.c
-	ar rcs ./bin/libst0.a ./bin/st0.o
 
 test:
 	@mkdir -p ./bin
--- src/st0.h
+++ src/st0.h
@@ -11,73 +11,28 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #ifndef ST0_HEADER
 #define ST0_HEADER
 
+#include <stdlib.h>
 #include <stdint.h>
 
-/* type declaration */
-#ifdef ST0_INTERNAL
-
-typedef unsigned char char8_t;
-typedef float float32_t;
-typedef double float64_t;
-
-typedef enum {
-    ST0_TYPE_LIST_UINT32,
-    ST0_TYPE_LIST_INT32,
-    ST0_TYPE_LIST_UINT64,
-    ST0_TYPE_LIST_INT64,
-    ST0_TYPE_LIST_FLOAT32,
-    ST0_TYPE_LIST_FLOAT64,
-    ST0_TYPE_STRING_UTF8,
-    ST0_TYPES_COUNT
-} st0_type;
-
 typedef struct {
-    st0_type type;
     void* data_ptr;
     uint32_t size;
 } st0_list;
 
-typedef struct {
-    st0_type type;
-    void* data_ptr;
-    uint32_t allocated_bytes;
-    uint32_t used_bytes;
-} st0_string;
+#define st0_list_create(type, size) \
+{                                   \
+    malloc(sizeof(type) * (size)),  \
+    (size)                          \
+}
 
-#else
+void st0_list_destroy(st0_list* list_ptr) {
+    free(list_ptr->data_ptr);
+}
 
-typedef void st0_list;
-typedef void st0_string;
+#define st0_list_set(list, type, pos, val) \
+    ((type*)(list.data_ptr))[pos] = (val)
 
-#endif
-/* end of type declaration */
-
-/* list functions */
-void     st0_list_destroy(st0_list* list_ptr);
-uint32_t st0_list_get_size(st0_list* list_ptr);
-void     st0_list_set_value(st0_list* list_ptr, uint32_t pos, void* value_ptr);
-void     st0_list_get_value(st0_list* list_ptr, void* buffer_ptr, uint32_t pos);
-void     st0_list_push_back(st0_list* list_ptr, void* value_ptr);
-void     st0_list_pop_back(st0_list* list_ptr, void* buffer_ptr);
-void     st0_list_push(st0_list* list_ptr, uint32_t pos, void* value_ptr);
-void     st0_list_pop(st0_list* list_ptr, void* buffer_ptr, uint32_t pos);
-
-st0_list* st0_list_uint32_create(uint32_t size);
-st0_list* st0_list_int32_create(uint32_t size);
-st0_list* st0_list_uint64_create(uint32_t size);
-st0_list* st0_list_int64_create(uint32_t size);
-st0_list* st0_list_float32_create(uint32_t size);
-st0_list* st0_list_float64_create(uint32_t size);
-/* end of list functions */
-
-/* string functions */
-void     st0_string_destroy(st0_string* string_ptr);
-uint32_t st0_string_get_allocated_bytes(st0_string* string_ptr);
-uint32_t st0_string_get_used_bytes(st0_string* string_ptr);
-void     st0_string_assign_to_literal(st0_string* string_ptr, const char* literal_ptr);
-void     st0_string_get_char(st0_string* string_ptr, void* buffer, uint32_t pos);
-
-st0_string* st0_string_utf8_create(uint32_t allocated_bytes);
-/* end of string functions */
+#define st0_list_get(list, type, pos) \
+    ((type*)(list.data_ptr))[pos]
 
 #endif
